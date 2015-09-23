cbc57ba28094b5946b7136c4f2caab694c49edf2
Juan Daniel Lopez Serna <juan.daniel.lopez.serna@ericsson.com>
2015-09-23 15:40:21 +0100

    Notes and links can now be pushed

--- pushbullet/PushBulletIF.py
+++ pushbullet/PushBulletIF.py
@@ -6,13 +6,25 @@ class PushBulletIF:
     def __init__(self, token):
         self.token = token
 
-    def send(self, contents):
-        if type(contents) == list:
-            contents = '\n'.join(contents)
+    def push_note(self, title, body):
+        if type(body) == list:
+            body = '\n'.join(body)
         msg = {
             'type'  : 'note',
-            'title' : 'Tsdb update',
-            'body'  : contents
+            'title' : title,
+            'body'  : body
+        }
+        requests.post('https://api.pushbullet.com/v2/pushes', data=msg, auth=(self.token, ""))
+
+
+    def push_link(self, title, body, url):
+        if type(body) == list:
+            body = '\n'.join(body)
+        msg = {
+            'type'  : 'link',
+            'title' : title,
+            'body'  : body,
+            'url'   : url
         }
         requests.post('https://api.pushbullet.com/v2/pushes', data=msg, auth=(self.token, ""))
 
